28407bb209e2b9d3e1a887bc0357e92dea6f0b21
Jake <Conway@users.noreply.github.com>
2019-04-27 18:53:35 -0400

    bold help commands

--- help.py
+++ help.py
@@ -1,15 +1,15 @@
 import config
 
 def help_text(team):
-    commands = ["• '@(user)++': add points to a user (e.g. {ping}++) ",
-                "• '@(user)--': take points from a user (e.g. {ping}--)",
-                "• '@(user)==': get current point total of a user (e.g. {ping}==)",
-                "• '#(thing)++': give points to a thing (e.g. #jake++)",
-                "• '#(thing)--': take points from a thing (e.g. #jake--)",
-                "• '#(thing)==': get current point total of a thing (e.g. #jake==)",
-                "• '{ping} leaderboard': get the current high scoring people and things",
-                "• '{ping} loserboard': get the current low scoring people and things",
-                "• '{ping} feedback <feedback>': send feedback about this bot to its wrangler"]
+    commands = ["• *@(user)++*: add points to a user (e.g. {ping}++) ",
+                "• *@(user)--*: take points from a user (e.g. {ping}--)",
+                "• *@(user)==*: get current point total of a user (e.g. {ping}==)",
+                "• *#(thing)++*: give points to a thing (e.g. #jake++)",
+                "• *#(thing)--*: take points from a thing (e.g. #jake--)",
+                "• *#(thing)==*: get current point total of a thing (e.g. #jake==)",
+                "• *{ping} leaderboard*: get the current high scoring people and things",
+                "• *{ping} loserboard*: get the current low scoring people and things",
+                "• *{ping} feedback <feedback>*: send feedback about this bot to its wrangler"]
     formatted_commands = list()
     for command in commands:
         formatted_commands.append(command.format(ping=f"<@{team.bot_user_id}>"))
