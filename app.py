b2fc4bc2e8dc116e0000fc6240bad9e8c5766f3c
Jake <Conway@users.noreply.github.com>
2019-04-27 20:25:16 -0400

    move slack message handling into its own file

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 from flask import Flask, redirect, request
 from slackeventsapi import SlackEventAdapter
 from slackclient import SlackClient
-from operations import process_match, generate_string
-from leaderboard import generate_leaderboard
 from models import db, SlackTeam
-from help import help_text
-import re
-
-# get Slack secret
-operation_exp = re.compile(r"(<?@|#)(.+)(\+\+|\-\-|==)")
+from slack_handler import process_incoming_message
 
 # flask init
 app = Flask(__name__)
@@ -55,65 +49,7 @@ def slack_auth():
 
 @slack.on("message")
 def handle_message(event_data, req):
-    # ignore retries
-    if req.headers.get('X-Slack-Retry-Reason'):
-        return "Status: OK"
-    # ignore bot messages
-    if 'subtype' in event_data['event'] and event_data['event']['subtype'] == 'bot_message':
-        return "Status: OK"
-
-    event = event_data['event']
-    message = event.get('text').lower()
-    user = event.get('user').lower()
-    channel = event.get('channel')
-
-    # load/update team
-    team = SlackTeam.query.filter_by(id=event_data['team_id']).first()
-    team.update_last_access()
-    db.session.add(team)
-    db.session.commit()
-
-    operation_match = operation_exp.match(message)
-    if operation_match:
-        thing, operation = process_match(operation_match, user, team)
-        db.session.add(thing)
-        db.session.commit()
-        output = generate_string(thing, operation)
-        team.slack_client().api_call(
-            "chat.postMessage",
-            channel=channel,
-            text=output
-        )
-        print("Processed " + thing.item)
-    elif "leaderboard" in message and team.bot_user_id.lower() in message:
-        team.slack_client().api_call(
-            "chat.postMessage",
-            channel=channel,
-            blocks=generate_leaderboard()
-        )
-        print("Processed leaderboard for team " + team.id)
-    elif "loserboard" in message and team.bot_user_id.lower() in message:
-        team.slack_client().api_call(
-            "chat.postMessage",
-            channel=channel,
-            blocks=generate_leaderboard(losers=True)
-        )
-        print("Processed loserboard for team " + team.id)
-    elif "help" in message and team.bot_user_id.lower() in message:
-        team.slack_client().api_call(
-            "chat.postMessage",
-            channel=channel,
-            blocks=help_text(team)
-        )
-        print("Processed help for team " + team.id)
-    elif "feedback" in message and team.bot_user_id.lower() in message:
-        print(message)
-        team.slack_client().api_call(
-            "chat.postMessage",
-            channel=channel,
-            text="Thanks! For a more urgent response, please email " + app.config['SUPPORT_EMAIL']
-        )
-    return "OK", 200
+    process_incoming_message(event_data, req)
 
 
 if __name__ == '__main__':
---
+++ slack_handler.py
@@ -0,0 +1,70 @@
+from operations import process_match, generate_string
+from leaderboard import generate_leaderboard
+from help import help_text
+from models import db, SlackTeam
+import re
+import config
+
+operation_exp = re.compile(r"(<?@|#)(.+)(\+\+|\-\-|==)")
+
+
+def process_incoming_message(event_data, req):
+    # ignore retries
+    if req.headers.get('X-Slack-Retry-Reason'):
+        return "Status: OK"
+    # ignore bot messages
+    if 'subtype' in event_data['event'] and event_data['event']['subtype'] == 'bot_message':
+        return "Status: OK"
+
+    event = event_data['event']
+    message = event.get('text').lower()
+    user = event.get('user').lower()
+    channel = event.get('channel')
+
+    # load/update team
+    team = SlackTeam.query.filter_by(id=event_data['team_id']).first()
+    team.update_last_access()
+    db.session.add(team)
+    db.session.commit()
+
+    operation_match = operation_exp.match(message)
+    if operation_match:
+        thing, operation = process_match(operation_match, user, team)
+        db.session.add(thing)
+        db.session.commit()
+        output = generate_string(thing, operation)
+        team.slack_client().api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=output
+        )
+        print("Processed " + thing.item)
+    elif "leaderboard" in message and team.bot_user_id.lower() in message:
+        team.slack_client().api_call(
+            "chat.postMessage",
+            channel=channel,
+            blocks=generate_leaderboard()
+        )
+        print("Processed leaderboard for team " + team.id)
+    elif "loserboard" in message and team.bot_user_id.lower() in message:
+        team.slack_client().api_call(
+            "chat.postMessage",
+            channel=channel,
+            blocks=generate_leaderboard(losers=True)
+        )
+        print("Processed loserboard for team " + team.id)
+    elif "help" in message and team.bot_user_id.lower() in message:
+        team.slack_client().api_call(
+            "chat.postMessage",
+            channel=channel,
+            blocks=help_text(team)
+        )
+        print("Processed help for team " + team.id)
+    elif "feedback" in message and team.bot_user_id.lower() in message:
+        print(message)
+        team.slack_client().api_call(
+            "chat.postMessage",
+            channel=channel,
+            text="Thanks! For a more urgent response, please email " + config.SUPPORT_EMAIL
+        )
+    return "OK", 200
