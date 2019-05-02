b0f1bb796dfce1e095d19aae43e92ca03ff4c209
Jake <Conway@users.noreply.github.com>
2019-05-02 00:37:32 -0400

    change to relative paths

--- plusplus/views.py
+++ plusplus/views.py
@@ -11,14 +11,14 @@ def index():
 
 @views.route('/privacy_policy')
 def privacy_policy():
-    with open("/plusplus/content/privacy.md", "r") as f:
+    with open("plusplus/content/privacy.md", "r") as f:
         text = markdown.markdown(f.read())
     return render_template("document.html", title="Privacy Policy", content=text)
 
 
 @views.route('/faq')
 def faq():
-    with open("/plusplus/content/faq.md", "r") as f:
+    with open("plusplus/content/fail.md", "r") as f:
         text = markdown.markdown(f.read())
     return render_template("document.html", title="FAQ", content=text)
 
@@ -30,13 +30,13 @@ def support():
 
 @views.route('/installed')
 def success():
-    with open("/plusplus/content/success.md", "r") as f:
+    with open("plusplus/content/success.md", "r") as f:
         text = markdown.markdown(f.read())
     return render_template("document.html", title="Install Complete!", content=text)
 
 
 @views.route('/not_installed')
 def failure():
-    with open("/plusplus/content/fail.md", "r") as f:
+    with open("plusplus/content/fail.md", "r") as f:
         text = markdown.markdown(f.read())
     return render_template("document.html", title="Install Failed", content=text)
