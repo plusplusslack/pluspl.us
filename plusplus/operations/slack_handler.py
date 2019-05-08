3ae19a92133884e62c3f7f0cf56cae5256c0b0fe
Jake <Conway@users.noreply.github.com>
2019-05-07 21:37:27 -0500

    allow for spaces in username matches

--- plusplus/operations/slack_handler.py
+++ plusplus/operations/slack_handler.py
@@ -5,7 +5,7 @@ from plusplus.models import db, SlackTeam, Thing
 from plusplus import config
 import re
 
-user_exp = re.compile(r"<@([A-Za-z0-9]+)>(\+\+|\-\-|==)")
+user_exp = re.compile(r"<@([A-Za-z0-9]+)> *(\+\+|\-\-|==)")
 thing_exp = re.compile(r"#(.+)(\+\+|\-\-|==)")
 
 def process_incoming_message(event_data, req):
