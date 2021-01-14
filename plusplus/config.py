d4cffb81156a9da3243752ed7ba425c3fd2b1a1e
Jake Conway <jake.h.conway@gmail.com>
2021-01-14 17:13:17 -0600

    Add the missing scopes for private groups

--- plusplus/config.py
+++ plusplus/config.py
@@ -4,7 +4,7 @@ VERSION = 0.1
 NAME = os.environ.get("NAME")
 SLACK_CLIENT_ID = os.environ.get('SLACK_CLIENT_ID')
 SLACK_CLIENT_SECRET = os.environ.get('SLACK_CLIENT_SECRET')
-SLACK_SCOPES = "chat:write,im:history,im:write,mpim:history,mpim:write,team:read,channels:history,app_mentions:read"
+SLACK_SCOPES = "chat:write,im:history,im:write,mpim:history,mpim:write,team:read,channels:history,app_mentions:read,groups:history,groups:read,groups:write"
 SLACK_OAUTH_URL = f"https://slack.com/oauth/v2/authorize?client_id={SLACK_CLIENT_ID}&scope={SLACK_SCOPES}"
 SLACK_SIGNING_SECRET = os.environ.get('SLACK_SIGNING_SECRET')
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
